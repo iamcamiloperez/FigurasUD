a553621107dd1b738635fb2447b62568fe29cf9f
Cristhian Camilo Prez Veloza <42283471+iamcamiloperez@users.noreply.github.com>
2020-02-29 11:17:01 -0500

    Archivos Web

--- FigurasUd/src/app/app.component.ts
+++ FigurasUd/src/app/app.component.ts
@@ -16,32 +16,30 @@ export class AppComponent {
   
   
   llenarDatos(value:string){
-    this.figura.limpiar();
-    this.figura.nombre = value;
+    this.figura.clean();
+    this.figura.name = value;
     this.anchoFigura = "0px";
     this.altoFigura = "0px";
   }
 
   onSubmit(datos:NgForm){    
-    this.figura.altura = datos.value["altura"];
+    this.figura.height = datos.value["height"];
     this.figura.base = datos.value["base"];
-    this.figura.calcular();
+    this.figura.calculate();
     this.anchoFigura = this.figura.base + 'px';
-    this.altoFigura = this.figura.altura + 'px';
+    this.altoFigura = this.figura.height + 'px';
     
     if(this.figura.base > 270){
       let b = this.figura.base;
-      let h = this.figura.altura;          
+      let h = this.figura.height;          
       h = (h*270/b);
       b = 270;
-      if(this.figura.nombre == 'triángulo'){
+      if(this.figura.name == 'triangle'){
         h = h/2;
         b = b/2;
       }    
       this.altoFigura = h.toString() +'px';
       this.anchoFigura = b.toString()+'px';
-      console.log(this.anchoFigura);
-      console.log(this.altoFigura);
     }    
   }
 
